7757db01e027a6a796f6465e3c0b9df617cd7eb1
hugosmoreira <hugo.n4@gmail.com>
2021-10-28 18:38:35 -0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,46 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
-  return <Layout>
-            <Hero showPerson />
-          </Layout>
+
+
+const IndexPage = ({data}) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
+  return (
+      <Layout>
+        <Hero showPerson />
+        <Posts posts={posts} title="Recent Published" />
+      </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do YYYY")
+          readTime
+          slug
+          image {
+            childrenImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
+
+
+
 
 export default IndexPage
