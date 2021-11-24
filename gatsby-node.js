911750b2380a65d4bbe32285321b1d19dd1ebb52
hugosmoreira <hugo.n4@gmail.com>
2021-11-23 17:46:33 -0800

    post templates graphql update queries

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,32 @@
-// const path = require('path')
+const path = require('path')
 
-// // create pages dynamically
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   const result = await graphql(`
-//     {
-//       allMdx {
-//         nodes {
-//           frontmatter {
-//             slug
-//           }
-//         }
-//       }
-      
-//     }
-//   `)
+// create pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+      categories: allMdx {
+        distinct(field: frontmatter___category)
+      }
+    }
+  `)
 
-//   result.data.allMdx.nodes.forEach(({ frontmatter: { slug } }) => {
-//     createPage({
-//       path: `/posts/${slug}`,
-//       component: path.resolve(`src/templates/post-template.js`),
-//       context: {
-//         slug,
-//       },
-//     })
-//   })
+  result.data.allMdx.nodes.forEach(({ frontmatter: { slug } }) => {
+    createPage({
+      path: `/posts/${slug}`,
+      component: path.resolve(`src/templates/post-template.js`),
+      context: {
+        slug,
+      },
+    })
+  })
 
   
-// }
+}
